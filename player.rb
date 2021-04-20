480093a1ee8bb656604cf1dcfa574bd58242451d
Avishek-45 <theabhi45@gmail.com>
2021-04-20 13:33:08 +0545

    1/Avishek-45/added_player_class

---
+++ player.rb
@@ -0,0 +1,17 @@
+class Player
+    attr_reader :player_1 , :player_2
+
+    def initialize
+        puts "Enter your name"
+        @player_1 = gets.chomp
+        puts "#{@player_1} is X"
+        puts "Enter your name"
+        @player_2 = gets.chomp
+        puts "#{player_2} is O"
+    end
+
+end
+
+players = Player.new
+
+
